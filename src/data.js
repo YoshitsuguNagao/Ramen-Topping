6c06ed732a4d27313a1b46189a491a6726d16b6b
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-01-22 15:10:10 +0100

    update data.js

--- src/data.js
+++ src/data.js
@@ -2,21 +2,42 @@ var toppingList = [
   {
     type: "egg",
     isTopping:true,
-    color: "yellow",
     point: 1,
     image: 'images/egg.png',
   },
   {
+    type: "naruto",
+    isTopping: true,
+    point: 1,
+    image:'images/naruto.png',
+  },
+  {
     type: "chasyu",
     isTopping: true,
-    color: "brown",
     point: 1,
     image:'images/chasyu.png',
   },
   {
+    type: "nori",
+    isTopping: true,
+    point: 1,
+    image:'images/nori.png',
+  },
+  {
+    type: "negi",
+    isTopping: true,
+    point: 1,
+    image:'images/negi.png',
+  },
+  {
+    type: "extra-noodle",
+    isTopping: true,
+    point: 1,
+    image:'images/extra-noodle.png',
+  },
+  {
     type: "tomato",
     isTopping: false,
-    color: "red",
     point: -999,
     image: 'images/tomato.png'
   },
